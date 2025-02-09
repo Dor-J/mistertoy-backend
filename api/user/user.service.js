5a714368cb2cfad06205dd69bcd1691f12bbbc5b
Dor-J <Mr.DorJosef@gmail.com>
2025-02-09 17:35:55 +0200

    Refactor user service controller and routes

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,75 +1,199 @@
-import fs from 'fs'
-import Cryptr from 'cryptr'
-
 import { utilService } from '../../services/util.service.js'
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
 
-const cryptr = new Cryptr(process.env.SECRET1 || 'secret-puk-1234')
+import { ObjectId } from 'mongodb'
+
 const users = utilService.readJsonFile('data/user.json')
 
 export const userService = {
   query,
   getById,
+  getByUsername,
   remove,
-  save,
+  update,
+  add,
 }
 
-function query() {
-  const usersToReturn = users.map((user) => ({
-    _id: user._id,
-    fullname: user.fullname,
-  }))
-  return Promise.resolve(usersToReturn)
-}
+// function query() {
+//   const usersToReturn = users.map((user) => ({
+//     _id: user._id,
+//     fullname: user.fullname,
+//   }))
+//   return Promise.resolve(usersToReturn)
+// }
+async function query(filterBy = {}) {
+  const criteria = _buildCriteria(filterBy)
+  try {
+    const collection = await dbService.getCollection('user')
 
-function getById(userId) {
-  var user = users.find((user) => user._id === userId)
-  if (!user) return Promise.reject('User not found!')
-
-  user = {
-    _id: user._id,
-    username: user.username,
-    fullname: user.fullname,
-    score: user.score,
-    isAdmin: user.isAdmin || false,
-  }
-
-  return Promise.resolve(user)
-}
-
-function remove(userId) {
-  //users = users.filter(user => user._id !== userId)
-  const idx = users.findIndex((user) => user._id === userId)
-  if (idx !== -1) users.splice(idx, 1)
-  return _saveUsersToFile()
-}
-
-function save(user) {
-  let userToUpdate = user
-  if (user._id) {
-    userToUpdate = users.find((_user) => user._id === _user._id)
-    userToUpdate.score = user.score
-    userToUpdate.isAdmin = user.isAdmin
-  } else {
-    userToUpdate._id = utilService.makeId(7)
-    users.push(userToUpdate)
-  }
-  const miniUser = {
-    _id: userToUpdate._id,
-    fullname: userToUpdate.fullname,
-    score: userToUpdate.score,
-    isAdmin: user.isAdmin,
-  }
-  return _saveUsersToFile().then(() => miniUser)
-}
-
-function _saveUsersToFile() {
-  return new Promise((resolve, reject) => {
-    const usersStr = JSON.stringify(users, null, 2)
-    fs.writeFile('data/user.json', usersStr, (err) => {
-      if (err) {
-        return console.error(err)
-      }
-      resolve()
+    var users = await collection.find(criteria).sort({ fullname: -1 }).toArray()
+    users = users.map((user) => {
+      delete user.password
+      return user
     })
-  })
+
+    return users
+  } catch (err) {
+    logger.error('No users found', err)
+    throw err
+  }
 }
+
+// function getById(userId) {
+//   var user = users.find((user) => user._id === userId)
+//   if (!user) return Promise.reject('User not found!')
+
+//   user = {
+//     _id: user._id,
+//     username: user.username,
+//     fullname: user.fullname,
+//     score: user.score,
+//     isAdmin: user.isAdmin || false,
+//   }
+
+//   return Promise.resolve(user)
+// }
+
+async function getById(userId) {
+  try {
+    const collection = await dbService.getCollection('user')
+
+    const user = await collection.findOne({
+      _id: ObjectId.createFromHexString(userId),
+    })
+    delete user.password
+    return user
+  } catch (err) {
+    logger.error(`Error finding user by ID: ${userId}`, err)
+    throw err
+  }
+}
+
+async function getByUsername(username) {
+  try {
+    const collection = await dbService.getCollection('user')
+
+    const user = await collection.findOne({ username })
+    delete user.password
+    return user
+  } catch (err) {
+    logger.error(`Error finding user by username: ${username}`, err)
+    throw err
+  }
+}
+
+// function remove(userId) {
+//   //users = users.filter(user => user._id !== userId)
+//   const idx = users.findIndex((user) => user._id === userId)
+//   if (idx !== -1) users.splice(idx, 1)
+//   return _saveUsersToFile()
+// }
+async function remove(userId) {
+  try {
+    const collection = await dbService.getCollection('user')
+
+    await collection.deleteOne({ _id: ObjectId.createFromHexString(userId) })
+  } catch (err) {
+    logger.error(`Error cannot remove user with id: ${userId}`, err)
+    throw err
+  }
+}
+
+// function save(user) {
+//   let userToUpdate = user
+//   if (user._id) {
+//     userToUpdate = users.find((_user) => user._id === _user._id)
+//     userToUpdate.score = user.score
+//     userToUpdate.isAdmin = user.isAdmin
+//   } else {
+//     userToUpdate._id = utilService.makeId(7)
+//     users.push(userToUpdate)
+//   }
+//   const miniUser = {
+//     _id: userToUpdate._id,
+//     fullname: userToUpdate.fullname,
+//     score: userToUpdate.score,
+//     isAdmin: user.isAdmin,
+//   }
+//   return _saveUsersToFile().then(() => miniUser)
+// }
+async function update(user) {
+  try {
+    // peek only updatable fields!
+    const userToSave = {
+      _id: ObjectId.createFromHexString(user._id),
+      username: user.username,
+      fullname: user.fullname,
+      score: user.score,
+      createdAt: user.createdAt,
+      updatedAt: Date.now(),
+    }
+
+    const collection = await dbService.getCollection('user')
+
+    await collection.updateOne({ _id: userToSave._id }, { $set: userToSave })
+
+    return userToSave
+  } catch (err) {
+    logger.error(`cannot update user ${user._id}`, err)
+    throw err
+  }
+}
+
+async function add(user) {
+  try {
+    // Validate that there are no such user:
+    const existUser = await getByUsername(user.username)
+    if (existUser) throw new Error('Username taken')
+
+    // peek only updatable fields!
+    const userToAdd = {
+      username: user.username,
+      password: user.password,
+      fullname: user.fullname,
+      score: user.score || 0,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    }
+    const collection = await dbService.getCollection('user')
+    await collection.insertOne(userToAdd)
+
+    return userToAdd
+  } catch (err) {
+    logger.error('cannot insert user', err)
+    throw err
+  }
+}
+
+function _buildCriteria(filterBy) {
+  const criteria = {}
+  if (filterBy.searchCriteria) {
+    const txtCriteria = { $regex: filterBy.searchCriteria, $options: 'i' }
+    criteria.$or = [
+      {
+        username: txtCriteria,
+      },
+      {
+        fullname: txtCriteria,
+      },
+    ]
+  }
+  if (filterBy.minBalance) {
+    criteria.balance = { $gte: filterBy.minBalance }
+  }
+
+  return criteria
+}
+
+// function _saveUsersToFile() {
+//   return new Promise((resolve, reject) => {
+//     const usersStr = JSON.stringify(users, null, 2)
+//     fs.writeFile('data/user.json', usersStr, (err) => {
+//       if (err) {
+//         return console.error(err)
+//       }
+//       resolve()
+//     })
+//   })
+// }
