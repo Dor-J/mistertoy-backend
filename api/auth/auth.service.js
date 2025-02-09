562824aff5cf92e447a2e9758a2b643e3c47d75b
Dor-J <Mr.DorJosef@gmail.com>
2025-02-09 17:08:34 +0200

    Set auth service , routes, services

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -1,56 +1,63 @@
 import fs from 'fs'
 import Cryptr from 'cryptr'
+import bcrypt from 'bcrypt'
 
-import { utilService } from '../../services/util.service.js'
+// const users = utilService.readJsonFile('data/user.json')
 
-const cryptr = new Cryptr(process.env.SECRET1 || 'secret-puk-1234')
-const users = utilService.readJsonFile('data/user.json')
+import { userService } from '../user/user.service.js'
+import { logger } from '../../services/logger.service.js'
 
 export const userService = {
-  checkLogin,
+  signup,
+  login,
   getLoginToken,
   validateToken,
 }
 
-function checkLogin({ username, password }) {
-  // You might want to remove the password validation for dev
-  var user = users.find(
-    (user) => user.username === username && user.password === password
-  )
+const cryptr = new Cryptr(process.env.SECRET1 || 'secret-puk-1234')
 
-  if (user) {
-    user = {
-      _id: user._id,
-      fullname: user.fullname,
-      isAdmin: user.isAdmin || false,
-      score: user.score,
-    }
-  }
-  return Promise.resolve(user)
+async function login(username, password) {
+  logger.debug(`auth.service - login with username: ${username}`)
+
+  const user = await userService.getByUsername(username)
+  if (!user) throw new Error('Invalid username or password')
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) throw new Error('Invalid username or password')
+
+  delete user.password
+  return user
+}
+
+async function signup(username, password, fullname) {
+  const saltRounds = 10
+
+  logger.debug(
+    `auth.service - signup with username: ${username}, fullname: ${fullname}`
+  )
+  if (!username || !password || !fullname) throw new Error('Missing details')
+
+  const hash = await bcrypt.hash(password, saltRounds)
+  return userService.add({ username, password: hash, fullname })
 }
 
 function getLoginToken(user) {
-  const str = JSON.stringify(user)
-  const encryptedStr = cryptr.encrypt(str)
-  return encryptedStr
+  const userInfo = {
+    _id: user._id,
+    fullname: user.fullname,
+    isAdmin: user.isAdmin || false,
+  }
+  return cryptr.encrypt(JSON.stringify(userInfo))
 }
 
 function validateToken(token) {
   if (!token) return null
-
-  const str = cryptr.decrypt(token)
-  const user = JSON.parse(str)
-  return user
-}
-
-function _saveUsersToFile() {
-  return new Promise((resolve, reject) => {
-    const usersStr = JSON.stringify(users, null, 2)
-    fs.writeFile('data/user.json', usersStr, (err) => {
-      if (err) {
-        return console.error(err)
-      }
-      resolve()
-    })
-  })
+  try {
+    const str = cryptr.decrypt(token)
+    const user = JSON.parse(str)
+    return user
+  } catch (err) {
+    console.log('Invalid login token')
+  }
+  return null
 }
