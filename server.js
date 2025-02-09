b5c0a72c23462f4c88a3150cc1a22544e13ab484
Dor-J <Mr.DorJosef@gmail.com>
2025-02-09 15:51:36 +0200

    Preparing to use api/ toy user auth seperate directory with routes

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-import path from 'path'
 import cors from 'cors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-
-import { toyService } from './services/toy.service.js'
-import { userService } from './services/user.service.js'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import { loggerService } from './services/logger.service.js'
+
+//import { toyService } from './services/toy.service.js'
+//import { userService } from './services/user.service.js'
 
 const app = express()
 
 // Express Config:
-const corsOptions = {
-  origin: [
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
-  ],
-  credentials: true,
-}
+if (process.env.NODE_ENV === 'production') {
+  // Express serve static files on production environment
+  app.use(express.static(path.resolve(__dirname, 'public')))
+  console.log('__dirname: ', __dirname)
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:8080',
+      'http://localhost:8080',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  }
 
-app.use(cors(corsOptions))
+  app.use(cors(corsOptions))
+}
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
@@ -29,6 +38,14 @@ app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 
 // Express Routing:
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { toyRoutes } from './api/toy/toy.routes.js'
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/toy', toyRoutes)
 
 // REST API for Toys
 app.get('/api/toy', (req, res) => {
