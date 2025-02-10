9b865021cc0364d6b17114a90e49d9af8b791961
Dor-J <Mr.DorJosef@gmail.com>
2025-02-10 16:38:49 +0200

    Connect mongo

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,15 @@
+import 'dotenv/config'
+const DB_NAME = process.env.VITE_MONGO_NAME
+const DB_CRED = process.env.VITE_MONGO_CRED
+
 export default {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName: 'mister_toy_db',
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL:
+    'mongodb+srv://' +
+    DB_NAME +
+    ':' +
+    DB_CRED +
+    '@proj-mister-toy.r6vo5.mongodb.net/?retryWrites=true&w=majority&appName=proj-mister-toy',
+  // dbName: 'mister_toy_db',
+  dbName: 'mister_toy',
 }
