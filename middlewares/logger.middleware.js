27cc4c57ab76253c92f0e77fa992837deae77a45
Dor-J <Mr.DorJosef@gmail.com>
2025-02-11 20:38:52 +0200

    Add small chages incuding changing name in setting

--- middlewares/logger.middleware.js
+++ middlewares/logger.middleware.js
@@ -2,7 +2,7 @@ import { loggerService } from '../services/logger.service.js'
 
 export async function log(req, res, next) {
   //loggerService.info('Req was made', req.route.path)
-  const { baseUrl, method, body, params } = req
-  loggerService.info(baseUrl, method, body, params)
+  const { baseUrl, method, body, route, params } = req
+  loggerService.info(baseUrl, method, body, route.path, params)
   next()
 }
